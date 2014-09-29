97413081e1287bc22f1d2ae00e8148c1ec5f93ba
Mishka Rogachev <mishkarogachev@gmail.com>
2014-09-29 18:00:52 +0400

    Basic main and menu items implimentation

---
+++ sources/views/menu_view/qml_menu_view.cpp
@@ -0,0 +1,5 @@
+#include "qml_menu_view.h"
+
+QmlMenuView::QmlMenuView()
+{
+}
---
+++ sources/views/menu_view/qml_menu_view.h
@@ -0,0 +1,10 @@
+#ifndef QML_MENU_VIEW_H
+#define QML_MENU_VIEW_H
+
+class QmlMenuView : public IMenuView
+{
+public:
+    QmlMenuView();
+};
+
+#endif // QML_MENU_VIEW_H
